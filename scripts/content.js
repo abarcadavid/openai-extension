4a93656019e67b0fb36d1a52bce65599c23796d8
abarcadavid <davidabarca.swe@protonmail.com>
2023-02-14 18:02:28 -0800

    Linting

--- scripts/content.js
+++ scripts/content.js
@@ -1,7 +1,7 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.message == 'inject') {
-    const {content} = request;
+  if (request.message === 'inject') {
+    const { content } = request;
     console.log(content);
-    sendResponse({status: 'success'});
+    sendResponse({ status: 'success' });
   }
 });
